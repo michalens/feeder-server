ca8d8531c4e85df6ede887df3cd77de213bbfbbd
michael <michael@michaels-MacBook-Air.local>
2019-09-19 18:52:53 +0300

    add opml parser and add to a db

--- controllers/opml-parser.js
+++ controllers/opml-parser.js
@@ -1,82 +1,87 @@
 const convert = require('xml-js')
+const RSSFeed = require('../models/rss-model').RSSFeed
 const Folder = require('../models/rss-model').Folder
-const api = require('./rss-ctrl')
+const fs = require('fs')
 
 function opmlUpload(req, res) {
-
   	const opmlFile = req.files.file;
   	opmlFile.mv(__dirname + `/../public/opmlUpload/${opmlFile.name}`, function(err) {
     if (err) {
       return res.status(500).send(err);
     }
 
-    res.json({file: `public/opmlUpload/${opmlFile.name}`});
+    res.json({file: `public/opmlUpload/${opmlFile.name}`})
+
+    opmlParser(__dirname + `/../public/opmlUpload/${opmlFile.name}`)
   });	
 }
 
+function opmlParser(file) {
+	if (file.search(/(\.opml$)/gm) === -1 ) {
+		console.log("Not an OPML file")
+		return false;
+	}
+	const xml = fs.readFileSync(file, 'utf8');
+	const result = convert.xml2js(xml, {compact: true})
 
+	outlineLoop(result.opml.body.outline)
+	fs.unlink(file, err => {
+		if (err) throw err;
+  console.log('successfully deleted', file);
+	})
+}
 
+function outlineLoop(obj, parentId) {
+	for (let feed in obj) {
+		const item = obj[feed]
+		if (!item._attributes) {
+			console.log(item)
+		} else {
+			if (item._attributes.type === 'folder') {
 
-// async function createFolder(obj) {
-// 	const folder = new Folder(obj)
+				const folder = new Folder({ 
+					title: item._attributes.title, 
+					parentFolder: parentId,
 
-// 	if (!folder) {
-//         return { success: false, error: "Error, folder not provided" }
-//     }
+				})
+				folder.save((err, folder) => {
+					if(err){
+					    console.log("Something went wrong,", err)
+					    return
+					}
+			      	if (parentId !== 0) {
+						Folder.findByIdAndUpdate(parentId, { $push: {folders: folder._id} }, (err, res)=>{
+							if (err) {console.log(err)}
+						})
+			      	}
+					outlineLoop(item.outline, folder._id)
+				})
+			} else if (item._attributes.type === 'rss' || item._attributes.type === 'atom') {
+				const { title, xmlUrl, htmlUrl } = item._attributes
+				
+				const feed = new RSSFeed({
+					title,
+					feedUrl: xmlUrl,
+					link: htmlUrl,
+					parentFolder: parentId
+				})
+				feed.save((err, feed) => {
+					if(err){
+					    console.log("Something went wrong,", err)
+					    return
+					}
+			      	if (parentId !== 0) {
+						Folder.findByIdAndUpdate(parentId, { $push: {feeds: feed._id} }, (err, res)=>{
+							if (err) {console.log(err)}
+						})
+			      	}
+				})
+			}
 
-//     folder
-//         .save()
-//         .then(() => {
-//             return {
-//                 success: true,
-//                 id: folder._id,
-//                 message: 'Folder added!',
-//             }
-//         })
-//         .catch(error => {
-//         	console.log(error)
-//             return {
-//                 error,
-//                 message: 'Folder not added!',
-//             }
-//         })
-// }
+		}
+	}
 
-// async function outlineLoop(obj) {
-// 	for (let feed in obj) {
-// 		const item = {}
-
-// 		if (obj[feed]._attributes.type === 'folder') {
-// 			item.title = obj[feed]._attributes.title
-// 			console.log(item.title)
-// 			// await createFolder(item)
-
-// 		} else if (obj[feed]._attributes.type === 'rss' || obj[feed]._attributes.type === 'atom') {
-// 			const { title, xmlUrl, htmlUrl } = obj[feed]._attributes
-// 			api.createFeed({body: {
-// 				title,
-// 				link: htmlUrl,
-// 				feedUrl: xmlUrl
-// 			}})
-// 			console.log(obj[feed]._attributes.title)
-// 		}
-// 	}
-// }
-
-// function opmlParser(file) {
-// 	if (file.search(/(\.opml$)/gm) === -1 ) {
-// 		console.log("Not an OPML file")
-// 		return false;
-// 	}
-// 	const xml = require('fs').readFileSync(file, 'utf8');
-// 	const result = convert.xml2js(xml, {compact: true})
-
-// 	outlineLoop(result.opml.body.outline)
-
-
-// }
-
-// opmlParser('feedlist1.opml')
+}
 
 module.exports = {
 	opmlUpload
--- models/rss-model.js
+++ models/rss-model.js
@@ -6,12 +6,12 @@ const folderSchema = new Schema({
         type: String, 
         required: true 
     },
+    parentFolder: { type: Schema.Types.ObjectId, ref: 'Folder' },
     folders: [{ type: Schema.Types.ObjectId, ref: 'Folder' }],
     feeds: [{ type: Schema.Types.ObjectId, ref: 'RSSFeed' }]
 })
 
-const rssFeedSchema = new Schema(
-  	{
+const rssFeedSchema = new Schema({
         feedUrl: { 
         	type: String, 
         	required: true 
@@ -31,10 +31,10 @@ const rssFeedSchema = new Schema(
         items: {
             type: Array,
             required: false
-        }
+        },
+        parentFolder: { type: Schema.Types.ObjectId, ref: 'Folder' },
 
-    }
-)
+    })
 
 const RSSFeed = mongoose.model('RSSFeed', rssFeedSchema);
 const Folder = mongoose.model('Folder', folderSchema);
